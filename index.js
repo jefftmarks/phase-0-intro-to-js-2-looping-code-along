c9560183aa5974525388d829dbab950376e40f91
Jeff Marks <jeff.t.marks@gmail.com>
2022-06-14 13:58:50 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = [];
+const thankyou = [];
+
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    thankyou[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+  }
+  return thankyou;
+};
+
+
+function countDown(integer) {
+  while (integer >= 0) {
+    console.log(integer--);
+  }
+}
