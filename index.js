00a1c09bcc2b8c6637b132f9a0680171c22ee71a
Jeff Marks <jeff.t.marks@gmail.com>
2022-07-19 12:10:55 -0400

    commit tree 4680cd09154caf4115dbb85dc18eb30622df1b16

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-const names = [];
-const thankyou = [];
+// const names = [];
+// const thankyou = [];
 
-function writeCards(names, event) {
+// function writeCards(names, event) {
+//   for (let i = 0; i < names.length; i++) {
+//     thankyou[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+//   }
+//   return thankyou;
+// };
+
+
+// function countDown(integer) {
+//   while (integer >= 0) {
+//     console.log(integer--);
+//   }
+// }
+
+// const writeCards = (names, event) => {
+//   const thank_you = []
+//   for (let i = 0; i < names.length; i++) {
+//     thank_you[i] = ` Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+//   }
+//   return thank_you;
+// }
+
+const writeCards = (names, event) => {
+  const thank_you = [];
   for (let i = 0; i < names.length; i++) {
-    thankyou[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
-  }
-  return thankyou;
-};
+    thank_you.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  };
+  return thank_you;
+}
 
-
-function countDown(integer) {
-  while (integer >= 0) {
-    console.log(integer--);
-  }
+const countDown = int => {
+  let i = int;
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  };
 }
